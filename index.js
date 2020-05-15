9c8270d35a452a8c8f40a893a622920f8fe23bef
Daniel Hoesle <d1@PacBook-Mro.local>
2020-05-15 12:04:18 -0600

    completed stying section of sprint as well as tasks 1-3 and 5

--- index.js
+++ index.js
@@ -207,12 +207,13 @@ const artists = [
 
 (1) Name of the first artist in the array
 (2) Bio of the third artist in the array */
-
+console.log(artists[0]);
+console.log(artists[2].bio);
 
 
 /* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
-
-
+artists[8].name = 'Vincent Van Gogh';
+console.log(artists[8].name)
 
 /* Task 3: Create a function called `getArtistByIndex` that takes two arguments:
  *     (1) artists array
@@ -222,10 +223,11 @@ const artists = [
  * For example, if getArtistByIndex is invoked with the inventory and the number 0,
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
-function getArtistByIndex(id, name) {
-    /* code here */
+function getArtistByIndex(array, index) {
+    const artistVal = array[index];
+    return `The artist at index ${artistVal.id} is ${artistVal.name}.`
   }
-  
+  console.log(getArtistByIndex(artists, 0));
   /**
 
 
@@ -237,21 +239,30 @@ function getArtistByIndex(id, name) {
  * For example, if removeArtist is invoked with the data and the number 0,
  * it will remove Amedeo Modigliani from our dataset.
 */
-function removeArtist(/*code here*/) {
-    /* code here */
+function removeArtist(array, index) {
+    array.splice(index,index);
+    
   }
-  
+  removeArtist( artists, 0)
+  console.log(artists[0]);
+
   /**
 
 
 /* Task 5: Create a function called lotsOfArt() that takes artists as an argument and returns an array with names of artists who painted more than 100 paintings */
 
-function lotsOfArt(/* Code here */){
+function lotsOfArt(array){
+  const artistsNames = [];
+  for (i=0; i<= array.length-1; i++){
+    if (array[i].paintings > 100){
+      artistsNames.push(array[i].name);
 
+    }
+  }return artistsNames;
     /* Code here */
 
   }
-
+  console.log(lotsOfArt(artists))
 
 /* Task 6: Create a function called `addArtist` that can accept an array of information and add it to the artists array. Then, Add a 21st artist to the array (you) with custom information! 👩‍🎨👨‍🎨
 
